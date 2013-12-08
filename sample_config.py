2f8cc3e67f1785890455c96f989c139431236e36
Anirudh Ranganath <anipub@hotmail.com>
2013-12-07 19:01:30 -0800

    Added sample config

---
+++ sample_config.py
@@ -0,0 +1,14 @@
+import os
+
+REPO_PATH = '/home/anirudh/548/mashventure'
+
+#DATA_PATH
+DATA_DIR_PATH = '/home/anirudh/548/data/'
+RIDB_DATA_XML_PATH = os.path.join(DATA_DIR_PATH,'ridbData.xml')
+
+#MASHVENTURE KEYS
+MV_FORM_SECRET_KEY = os.environ['MASHVENTURE_SECRET_KEY'] #set as environment variable
+
+#URLS
+
+#API END POINTS
